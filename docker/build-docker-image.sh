89dd847230bf6598dfb5442adeddcf9755245cd9
hhoefener <11026728+hhoefener@users.noreply.github.com>
2020-08-05 10:56:13 +0200

    added new Dockerfile

--- docker/build-docker-image.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Copyright (c) Fraunhofer MEVIS, Germany. All rights reserved.
-# **InsertLicense** code
-
-cd "$(dirname "$0")"
-
-docker build -t oswe-clusterhelloworld:latest .
