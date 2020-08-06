5ab09a15cc538285108919182adac1c58119c4f2
hhoefener <11026728+hhoefener@users.noreply.github.com>
2020-08-06 09:12:40 +0200

    added comment how to execute all notebooks

--- run_local.sh
+++ run_local.sh
@@ -11,4 +11,13 @@ docker run \
     idc-pathomics-use-case-1 \
     --NotebookApp.password=sha1:e94a79bd53f4:2213431eb9752b66e1734ca81548c339c5eb127e
 
+# to run all notebooks non-interactively and save the results, use
+#docker run \
+#    --rm \
+#    -u $(id -u ${USER}):$(id -g ${USER}) \
+#    -v SOMEPATH:/output_data \
+#    --entrypoint jupyter \
+#    idc-pathomics-use-case-1 \
+#    nbconvert --execute --output-dir=/output_data src/*.ipynb
+
 cd - > /dev/null # move back to previous directory
