df8e066663f1df71022b311c6b3dc38b64640813
dschacherer <daniela.schacherer@mevis.fraunhofer.de>
2020-09-03 11:57:09 +0200

    removed empty files

--- src/testing.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from evaluation.evaluation import per_slide_evaluation
-from data.data_set import Dataset
-
-if __name__ == '__main__':
-    test_dataset = Dataset('/home/dschacherer/Schreibtisch/testdata_out/csv_test_norm_cancer.csv', num_classes=3)
-    res=per_slide_evaluation('dummy', test_dataset)
-    print(res)
