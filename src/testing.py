f763513e0683db6ecefabebbfc45be3e27ad8927
dschacherer <daniela.schacherer@mevis.fraunhofer.de>
2020-09-24 15:15:36 +0200

    dev

--- src/testing.py
+++ src/testing.py
@@ -5,9 +5,21 @@ from evaluation.roc import evaluate_by_roc_curve
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    test_dataset = Dataset('/home/dschacherer/Schreibtisch/testdata_out/csv_test_norm_cancer.csv', num_classes=3)
-    res=per_slide_evaluation('dummy', test_dataset)
-    print(res)
-    fig = plt.figure()
-    evaluate_by_roc_curve(fig, res)
-    plt.show()
+    #test_dataset = Dataset('/home/dschacherer/Schreibtisch/testdata_out/csv_test_norm_cancer.csv', num_classes=3)
+    #res=per_slide_evaluation('dummy', test_dataset)
+    #print(res)
+    #fig = plt.figure()
+    #evaluate_by_roc_curve(fig, res)
+    #plt.show()
+
+    import json
+    import matplotlib.pyplot as plt
+    with open('/home/dschacherer/Schreibtisch/training_history.json', 'r') as json_file: 
+        history = json.load(json_file)
+        plt.plot(history['loss'], label='training loss')
+        plt.plot(history['val_loss'], label='validation loss')
+        plt.title('Training and validation loss')
+        plt.xlabel('epoch')
+        plt.ylabel('cross entropy loss')
+        plt.legend(loc='upper right')
+        plt.savefig('/home/dschacherer/Schreibtisch/loss.png')
