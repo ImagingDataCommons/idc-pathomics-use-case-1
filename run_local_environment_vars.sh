e82a4a3e97906a640b612362f1896fb45369d1f2
DanielaSchacherer <daniela.schacherer@web.de>
2020-11-09 11:36:40 +0000

    removed run local and dockerfiles

--- run_local_environment_vars.sh
+++
@@ -1,4 +0,0 @@
-# TO BE MODIFIED BY THE USER: To run the notebook locally in a docker you need to specify here your local input and output folder that shoud be mounted within the docker
-
-export IDC_PATHOMICS_USE_CASE_1_INPUT_DATA_DIR='/home/dschacherer/Schreibtisch/testdata_in'
-export IDC_PATHOMICS_USE_CASE_1_OUTPUT_DATA_DIR='/home/dschacherer/Schreibtisch/testdata_out'
