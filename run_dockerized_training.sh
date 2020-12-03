bebd30645a130c6af8cb40d83075ad9a3446ea7b
dschacherer <daniela.schacherer@mevis.fraunhofer.de>
2020-12-03 16:45:18 +0100

    dev

--- run_dockerized_training.sh
+++ run_dockerized_training.sh
@@ -16,7 +16,7 @@ docker run \
     -v /home/dschacherer/idc_output:/output_data \
     -e "IDC_OUTPUT_DATA_DIR=/output_data" \
     --entrypoint /bin/bash \
-    idc-pathomics-use-case-1 
+    idc-pathomics-use-case-1 \
     -c "jupyter nbconvert --to=script --output-dir=/tmp --RegexRemovePreprocessor.patterns=\"['^\%']\" training.ipynb ; PYTHONPATH=. python3 /tmp/training.py"
 
 cd - > /dev/null # move back to previous directory
