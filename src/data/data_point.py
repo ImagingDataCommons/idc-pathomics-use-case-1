912043171533a73d356eade71ffb6f70935a9601
dschacherer <daniela.schacherer@mevis.fraunhofer.de>
2021-03-12 16:41:04 +0100

    dev

--- src/data/data_point.py
+++ src/data/data_point.py
@@ -2,12 +2,12 @@ import os
 import numpy as np
 from tensorflow.keras.preprocessing.image import img_to_array, load_img
 from PIL.Image import Image
-from typing import List
+from typing import List, Union
 
 # Path structure is /some/dirs/[SVS-FILENAME-WITHOUT-EXTENSION]_files/magnification/[X]_[Y].jpeg
 
 class DataPoint:
-    def __init__(self, patch_path: str, reference_value: int) -> None:
+    def __init__(self, patch_path: str, reference_value: Union[int, List[int]]) -> None:
         self.patch_path = patch_path
         self.reference_value = reference_value
 
@@ -15,7 +15,7 @@ class DataPoint:
         img = load_img(self.patch_path, color_mode='rgb')
         return (img_to_array(img) / 127.5) - 1.0 # scale to [-1, 1]
 
-    def get_reference_value(self) -> int:
+    def get_reference_value(self) -> Union[int, List[int]]:
         return self.reference_value
 
     def get_slide_id(self) -> str:
