c8b58eee40147ddb4b8daeae7f38c6b48a25d20f
dschacherer <daniela.schacherer@mevis.fraunhofer.de>
2021-03-31 15:48:47 +0200

    dev

--- src/data/tile_sorting.py
+++ src/data/tile_sorting.py
@@ -48,7 +48,7 @@ def run_tile_sorting(source_folder: str, json_file: str, output_folder: str, sor
     patients_with_only_healthy_slide = set()
     for slide_folder in slides_meta:
         if slides_meta[slide_folder] == 'normal':
-            patientID = re.findall("TCGA-[0-9]{2}-[0-9]{4}", slide_folder)[0]
+            patientID = re.findall(r'TCGA-[0-9]{2}-[0-9]{4}', slide_folder)[0]
             patients_with_only_healthy_slide.add(patientID)
     print(patients_with_only_healthy_slide)
         
