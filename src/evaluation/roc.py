a95fb1833bfe2f16029ee18249e21f44878afc8d
DanielaSchacherer <daniela.schacherer@web.de>
2021-04-12 11:42:16 +0200

    dev

--- src/evaluation/roc.py
+++ src/evaluation/roc.py
@@ -251,5 +251,6 @@ class ROCAnalysis():
 
 
     def print_and_save_tabluar_results(self):
+        # TODO 
         print('tile_b',self.tile_auc, self.tile_ci)
         print('slide_b', self.auc, self.ci) 
