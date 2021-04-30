1646f066dcc9e2f68b8e359be0ddad8044346efb
dschacherer <daniela.schacherer@mevis.fraunhofer.de>
2021-04-30 16:40:22 +0200

    dev

--- src/data/tile_generation.py
+++ src/data/tile_generation.py
@@ -69,7 +69,7 @@ def _get_required_level(slide: openslide.OpenSlide, dz: DeepZoomGenerator, desir
     for current_level in range(dz.level_count-1, -1, -1):
         this_magnification = available_magnifications[0]/pow(2, dz.level_count - (level+1)) # compute current magnification depending on the recent level  
         print(this_magnification, round(this_magnification))
-        if round(this_magnification) != desired_magnification:  
+        if this_magnification != 20.0:  
             continue
         level = current_level
     return level 
