98d41ae040808819fbb4c3e5dff47025b540a52a
DanielaSchacherer <daniela.schacherer@web.de>
2021-05-04 12:39:22 +0200

    dev

--- src/data/tile_resizing.py
+++ src/data/tile_resizing.py
@@ -10,18 +10,19 @@ def copy_folder_structure(input_path, output_path):
             os.mkdir(structure)
 
 def copy_and_resize_tiles(tile_paths_in, output_folder):
-    for tile_path_in in tile_paths_in:
+    for tile_path_in in tile_paths_in[:10]:
         print(tile_path_in)
         len_out = len(output_folder.split('/'))
         tile_path_out = os.path.join(output_folder, '/'.join(tile_path_in.split('/')[len_out-1:]))
-        if not os.path.exists(tile_path_out):
-            img = Image.open(tile_path_in)
-            resized = img.resize((256,256))
-            resized.save(tile_path_out)
+        print(tile_path_out)
+        #if not os.path.exists(tile_path_out):
+            #img = Image.open(tile_path_in)
+            #resized = img.resize((128,128))
+            #resized.save(tile_path_out)
 
 if __name__ == '__main__':
     input_path = '/home/dschacherer/mnt/gaia/imageData/deep_learning/output/imaging-data-commons/idc-pathomics-use-case-1/tiles/'
-    output_path = '/home/dschacherer/mnt/gaia/imageData/deep_learning/output/imaging-data-commons/idc-pathomics-use-case-1/tiles_256/'
+    output_path = '/home/dschacherer/mnt/gaia/imageData/deep_learning/output/imaging-data-commons/idc-pathomics-use-case-1/tiles_128_5x/'
     copy_folder_structure(input_path, output_path)
     tile_paths_in = glob(input_path + '/*/*/*jpeg')
     copy_and_resize_tiles(tile_paths_in, output_path)
