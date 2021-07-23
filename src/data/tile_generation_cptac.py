c45ddfc3dd80699369c9dbe603186069a170e76b
dschacherer <daniela.schacherer@mevis.fraunhofer.de>
2021-07-23 18:33:13 +0200

    dev

--- src/data/tile_generation_cptac.py
+++ src/data/tile_generation_cptac.py
@@ -71,8 +71,8 @@ def _generate_tiles_for_slide(slidepath: str, metadata_path: str, output_folder:
 
 def _get_slide_id_from_slidepath(slidepath, metadata):
     slide_name = os.path.basename(slidepath) 
-    print(slide_name)
-    match = metadata[metadata['gcs_url'].str.match('*{x}$'.format(x=slide_name)) == True]['slide_id']
+    regex_string = r'.*{x}$'.format(x=slide_name)
+    match = metadata[metadata['gcs_url'].str.match(regex_string) == True]['slide_id']
     print(match)
 
 
